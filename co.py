83356ea9ebd45f2a2f09f0c3efe17a689c0c98da
jackjameswillis <78825639+jackjameswillis@users.noreply.github.com>
2022-11-02 13:33:41 +0000

    PBIL
    added PBIL to opt co

--- co.py
+++ co.py
@@ -39,3 +39,61 @@ class hillclimber:
       Es[t] = VE
 
     return (Vs, Es)
+
+class PBIL:
+
+  def __init__(self, N, lr, nlr):
+
+    self.P = np.ones(N) - 0.5
+
+    self.N = N
+
+    self.lr = lr
+
+    self.nlr
+  
+  def optimize(self, T, population_size, E):
+
+    best_inds = np.zeros((self.T, self.N))
+
+    best_inds_E = np.zeros(self.T)
+
+    for t in range(T):
+
+      population = (np.random.uniform(0, 1, (population_size, self.N)) <= self.P).astype(np.int)
+
+      best_ind = population[0]
+
+      best_ind_E = E(best_ind)
+
+      worst_ind = population[0]
+
+      worst_ind_E = E(worst_ind)
+
+      for ind in population:
+
+        ind_E = E(ind)
+
+        if ind_E <= best_ind_E:
+
+          best_ind = ind
+
+          best_ind_E = ind_E
+        
+        if ind_E >= worst_ind_E:
+
+          worst_ind = ind
+
+          worst_ind_E = ind_E
+        
+      P = P * (1 - self.lr) + best_ind * self.lr
+
+      neq = best_ind != worst_ind
+
+      for i in range(self.N):
+
+        if neq[i]:
+
+          P[i] = P[i] * (1 - self.nlr) + best_ind[i] * self.nlr
+
+    return (best_inds, best_inds_E)
