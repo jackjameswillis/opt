c93eccd172adc214ebddb93e6e39131bb87afe91
jackjameswillis <78825639+jackjameswillis@users.noreply.github.com>
2023-01-28 16:59:55 +0000

    sig jit
    trying to use sigs

--- co.py
+++ co.py
@@ -141,14 +141,14 @@ class rHNS:
 
     self.diag = 1 - np.diag(np.ones(N))
 
-  @jit
+  @jit(signature="float64")
   def E(self, V):
 
     V = V[:, np.newaxis]
 
     return -((V.T @ self.W) @ V) / 2
   
-  @jit
+  @jit(signature="float64")
   def hebb(self, V):
 
     V = V[:, np.newaxis]
