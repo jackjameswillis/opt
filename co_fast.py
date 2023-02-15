498f1d229d42c03b067d89bb9b865be2f76fe629
jackjameswillis <78825639+jackjameswillis@users.noreply.github.com>
2023-02-15 13:03:42 +0000

    update function, W parameter for hopfield
    Added a parameter to hopfield for weights and added a static function for hopfield asynchronous update rule

--- co_fast.py
+++ co_fast.py
@@ -1,21 +1,43 @@
+'''==============================================
+Imports
+=============================================='''
 # C compilable maths
 import numpy as np
 
 # Makes lots of code C
-from numba import int32, float32
+from numba import int32, float32, double, jit
 from numba.experimental import jitclass
 
+'''==============================================
+hopfield
+=============================================='''
+
 # Types must be given for C
-hopfield_decorators = [('N', int32)]
+# Reads return_type(given from V_type, W_type, i_type)
+update_signature = int32(int32[:], double[:,:], int32)
+
+# Update for individual units compiled to C
+@jit(update_signature, nopython=True)
+def update(V, W, i):
+
+    return np.sign(V[i] @ W[i])
+
+# Types must be given for C
+hopfield_decorators = [('N', int32),
+                        'W', double[:,:]]
 
 # Types are given so that class code can be compiled in C
 @jitclass(hopfield_decorators)
 class hopfield(object):
 
-    def __init__(self, N):
+    def __init__(self, N, W):
 
         self.N = N
 
+    
+'''==============================================
+Multiple-Dimensional Knapsack Problem (mdkp)
+=============================================='''
 # Types must be given for C
 mdkp_decorators = [('N', int32)]
 
